402e2668eadc23715e1de3036337f3692252c4e3
Christopher Ronca <christopherronca@gmail.com>
2023-02-23 20:30:31 -0700

    Login/Logout and favicon added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,42 @@
-function NavBar({showShoppingCart, setShowShoppingCart}) {
+import React, { useState } from 'react';
 
-    return ( 
-        <div className="Nav-Bar-Shopping-Cart">
-            <i className="fa fa-shopping-cart" onClick={() => setShowShoppingCart(!showShoppingCart)}></i>
-        </div>
+function NavBar({showShoppingCart, setShowShoppingCart}) {
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false)
+    
+    const handleShowShoppingCart = () => {
+        setShowShoppingCart(!showShoppingCart)
+    }
+
+    const setUserLoggedIn = () => {
+        setIsUserLoggedIn(true)
+    }
+
+    const setUserLoggedOut = () => {
+        setShowShoppingCart(false)
+        setIsUserLoggedIn(false)
+    }
+
+    return (
+        <>
+            {(isUserLoggedIn) ?
+                <>
+                    <div className="Nav-Bar-Shopping-Cart">
+                        <i className="fa fa-shopping-cart" onClick={handleShowShoppingCart}></i>
+                    </div>
+                    <div className="Nav-Bar-User-Login">
+                        <i className="fa fa-sign-out" title="Log-Out" onClick={setUserLoggedOut}></i>
+                    </div>
+                </> :
+                <>
+                    <div className="Nav-Bar-Sign-Up">
+                        <i className="fa fa-user-plus" title="Signup"></i>
+                    </div>
+                    <div className="Nav-Bar-User-Login">
+                        <i className="fa fa-sign-in" title="Log-In" onClick={setUserLoggedIn}></i>
+                    </div>
+                </>
+            }
+        </> 
      );
 }
 
